b1de80a4d1b4330b4c5de667cf64c331613bb9f3
Sherman Rofeman <skippyr.developer@gmail.com>
2024-03-26 10:26:11 -0300

    add error functions

--- src/dirls.c
+++ src/dirls.c
@@ -1,6 +1,30 @@
 #include "dirls.h"
 
+static int g_exitCode = 0;
+
+static void throwError(char *format, ...)
+{
+    va_list arguments;
+    va_start(arguments, format);
+    fprintf(stderr, "%s: ", PROGRAM_NAME);
+    vfprintf(stderr, format, arguments);
+    fprintf(stderr, "\n");
+    va_end(arguments);
+    exit(1);
+}
+
+static void writeError(char *format, ...)
+{
+    va_list arguments;
+    va_start(arguments, format);
+    fprintf(stderr, "%s: ", PROGRAM_NAME);
+    vfprintf(stderr, format, arguments);
+    fprintf(stderr, "\n");
+    va_end(arguments);
+    g_exitCode = 1;
+}
+
 int main(void)
 {
-    return 0;
+    return g_exitCode;
 }
--- src/dirls.h
+++ src/dirls.h
@@ -1,1 +1,32 @@
+#include <grp.h>
+#include <pwd.h>
+
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define PROGRAM_NAME "dirls"
+#define PROGRAM_VERSION "v2.0.0"
+
+enum CredentialType
+{
+    CredentialType_User,
+    CredentialType_Group
+};
+
+struct Credential
+{
+    char *name;
+    unsigned int id;
+    struct Credential *lowerCredential;
+    struct Credential *higherCredential;
+}; /* x86_64 Size: 32B Alignment: 8B
+    * x86    Size: 16B Alignment: 4B */
+
+static struct Credential *getCredentialByID(struct Credential **tree, int type, unsigned int id);
+static void deallocateCredentialTree(struct Credential *tree);
+
+static void *allocateHeapMemory(size_t size);
+static void deallocateHeapMemory(void *allocation);
+static void throwError(char *format, ...);
+static void writeError(char *format, ...);
